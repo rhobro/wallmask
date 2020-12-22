52c10aec75c44417c89da40ab208237767504b25
Rohan Mathew <robozmat@gmail.com>
2020-12-22 15:53:51 +0000

    Successfully configured go_ibm_db

--- cmd/wmindex/main.go
+++ cmd/wmindex/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/ibmdb/go_ibm_db"
 	"net/http"
 	"net/url"
 )
@@ -14,6 +17,14 @@ func init() {
 	}
 }
 
-func main() {
+const dsn = "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-lon02-04.services.eu-gb.bluemix.net;PORT=50001;PROTOCOL=TCPIP;UID=lzx36405;PWD=8k11s2d98lhk81^p;Security=SSL;"
 
+var DB *sql.DB
+
+func main() {
+	var err error
+	DB, err = sql.Open("go_ibm_db", dsn)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to open connection to DB: %s\n", err))
+	}
 }
